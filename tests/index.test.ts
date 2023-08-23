8ef67d2e87124b3926eecb2374eb8e5f7de62122
Samuel Pengelly <pengellysj@binnies.com>
2023-08-23 10:11:42 +0100

    Mock dates for test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,38 +1,28 @@
-import {describe, expect, test} from '@jest/globals';
-import {DateTime} from "luxon";
-import {parseCollectionDay} from "../util";
+import { describe, expect, test, beforeAll } from "@jest/globals";
+import { DateTime, Settings } from "luxon";
+import { parseCollectionDay } from "../util";
 
 describe("parseCollectionDay", () => {
-    test('Happy path', () => {
-        const actual = parseCollectionDay("Thursday, August 24");
-        const expected = DateTime.fromObject({
-            year: DateTime.now().year,
-            month: 8,
-            day: 24,
-            hour: 7
-        });
-        expect(actual.toISO()).toEqual(expected.toISO());
-    });
+  beforeAll(() => {
+    const expectedNow = DateTime.local(2023, 8, 23, 12, 30);
+    Settings.now = () => expectedNow.toMillis();
+  });
 
-    test('Single digit day', () => {
-        const actual = parseCollectionDay("Saturday, December 2");
-        const expected = DateTime.fromObject({
-            year: DateTime.now().year,
-            month: 12,
-            day: 2,
-            hour: 7
-        });
-        expect(actual.toISO()).toEqual(expected.toISO());
-    });
+  test("Happy path", () => {
+    const actual = parseCollectionDay("Thursday, August 24");
+    const expected = DateTime.local(2023, 8, 24, 7);
+    expect(actual.toISO()).toEqual(expected.toISO());
+  });
 
-    test('Next year', () => {
-        const actual = parseCollectionDay("Friday, January 5");
-        const expected = DateTime.fromObject({
-            year: DateTime.now().year + 1,
-            month: 1,
-            day: 5,
-            hour: 7
-        });
-        expect(actual.toISO()).toEqual(expected.toISO());
-    });
-})
+  test("Single digit day", () => {
+    const actual = parseCollectionDay("Saturday, December 2");
+    const expected = DateTime.local(2023, 12, 2, 7);
+    expect(actual.toISO()).toEqual(expected.toISO());
+  });
+
+  test("Next year", () => {
+    const actual = parseCollectionDay("Friday, January 5");
+    const expected = DateTime.local(2024, 1, 5, 7);
+    expect(actual.toISO()).toEqual(expected.toISO());
+  });
+});
